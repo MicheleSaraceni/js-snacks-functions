51084eca4dfd08957f9eed5685743622f8fe4b6d
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-06 17:35:36 +0100

    ended 03 - conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function cercavocali (stringa){
+    let nvocali = 0;
+    for (i = 0; stringa.length > i; i++){
+        if(stringa[i].toLowerCase() === "a" || stringa[i].toLowerCase() === "e" || stringa[i].toLowerCase() === "i" || stringa[i].toLowerCase() === "o" || stringa[i].toLowerCase() === "u"){
+            console.log("Trovata vocale: " + stringa[i] + " in " + i + " posizione.");
+            nvocali++;
+            }
+        }
+    return nvocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const n = cercavocali (word);
+console.log("Il numero di vocali presenti è: " + n);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
