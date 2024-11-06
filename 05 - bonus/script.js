511b791ea1cc67897756ccf9bf0903b95cbf061a
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-06 17:54:36 +0100

    ended 05 - bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto (nome){
+    const orario = new Date();
+    const hours = orario.getHours();
+    const minutes = orario.getMinutes();
+    if(hours >= 4 && hours < 13){
+        console.log ("Buongiorno " + nome + ", sono le " + hours + ":" + minutes);    
+    } else if(hours >= 13 && hours <= 17){
+        console.log ("Buon pomerigio " + nome + ", sono le " + hours + ":" + minutes);
+    } else{
+        console.log ("Buona sera " + nome + ", sono le " + hours + ":" + minutes);
+    }
+         
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const nome = prompt("Inserisci la persona da salutare");
+saluto (nome);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
