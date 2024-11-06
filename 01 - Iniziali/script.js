b15af4175d9fab4d2abd174f31aaf7cbd3357863
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-06 15:48:48 +0100

    ended 01 - Iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstcaracter(namesarray){
+    const newarray = [];
+    for (i = 0; namesarray.length > i; i++){
+        newarray[i] = namesarray[i][0];
+    }
+    return newarray;
+} 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const namesletter = firstcaracter(names)
+console.log (namesletter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
